d7bf638bddaa5453feab0f13dff39d7fbc83f583
fnzadhitya <137802138+fnzadhitya@users.noreply.github.com>
2023-07-01 22:10:51 +0700

    update sabtu malam

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function user()
-{
-	var
-}
+
+const btnForm = document.querySelector('.btn-form');
+const userBox = document.querySelector('#user-box')('create-box');
+
+document.querySelector('#user-button').onclick = () => {
+	btnForm.classList.toggle('active');
+};
